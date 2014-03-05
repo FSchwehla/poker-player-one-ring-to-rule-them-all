5e6c88049c45afec621821b5359e971511e21f74
DeVill <devillmeister@gmail.com>
2014-03-05 13:04:04 -0800

    Initial commit

---
+++ calc_cards.py
@@ -0,0 +1,45 @@
+import math
+
+HIGH_CARD_VALUES = {
+    'A': 10,
+    'K': 8,
+    'Q': 7,
+    'J': 6
+}
+
+HIGH_CARD_NUM = {
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'J': 11
+}
+
+
+def _card_to_value(card):
+    card_value = -1
+    rank = card['rank']
+    try:
+        card_num = int(rank)
+        card_value = card_num/2.0
+    except ValueError:
+        pass
+    if card_value < 0:
+        rank = rank.upper()
+        card_value = HIGH_CARD_VALUES[rank]
+        card_num = HIGH_CARD_NUM[rank]
+    return card_num, card_value
+
+
+def get_value(hand):
+    card1_num, card1_value = _card_to_value(hand[0])
+    card2_num, card2_value = _card_to_value(hand[1])
+    total_value = max(card1_value, card2_value)
+    if card1_value == card2_value:
+        total_value += card2_value
+    if hand[0]['suit'] == hand[1]['suit']:
+        total_value += 2
+
+    diff = abs(card1_num - card2_num)
+    total_value -= min(diff, 5)
+
+    return int(math.ceil(total_value))
---
+++ player.py
@@ -0,0 +1,62 @@
+import random
+import calc_cards
+
+class Player:
+    VERSION = "Default Python folding player"
+
+    def betRequest(self, game_state):
+        print (game_state)
+        in_action=game_state['in_action']
+        me=game_state['players'][in_action]
+        current_buy_in=game_state['current_buy_in']
+
+        cards = me['hole_cards']
+        self.rank1 = None
+        self.rank2 = None
+        if len(cards) > 1:
+            self.rank1 = cards[0]['rank']
+            self.rank2 = cards[1]['rank']
+            value = calc_cards.get_value(cards)
+            if self.rank1 == self.rank2 and self.rank1 == 'A':
+                self.log_raise()
+                return 250
+            elif self.rank1 == self.rank2:
+                self.log_raise()
+                return 50
+            elif value < 8 and self.rank1 != 'A' and self.rank2 != 'A':
+                self.log_check()
+                return 0 # check only, no raise
+
+        minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
+
+        amount = current_buy_in - me['bet']
+        if amount > self.MAX_BET and self.rank1 != self.rank2:
+            print('CHECK only (HIGH bet)')
+            return 0 # fold
+
+        if len(cards) > 1:
+            r = minimum_raise + random.random()*10 if random.random()>0.3 else 0
+            amount += int(r)
+        self.log_call(amount)
+        return amount # always call
+
+    def showdown(self, game_state):
+        return True
+
+    def log_check(self):
+        if self.rank1:
+            print('{} {}: CHECK only'.format(self.rank1, self.rank2))
+        else:
+            print('CHECK only')
+
+    def log_call(self, amount):
+        if self.rank1:
+            print('{} {}: CALL: {}'.format(self.rank1, self.rank2, amount))
+        else:
+            print('CALL: {}'.format(amount))
+
+    def log_raise(self, amount):
+        if self.rank1:
+            print('{} {}: BET: {}'.format(self.rank1, self.rank2, amount))
+        else:
+            print('BET: {}'.format(amount))
