32a6f7c1d583b31f55f3ab69a00bd27f93cc2371
FSchwehla <florian.schwehla@datev.de>
2023-04-14 13:11:38 +0200

    Test

--- player.py
+++ player.py
@@ -4,89 +4,124 @@ import calc_cards
 class Player:
     VERSION = "One Ring"
 
-    MAX_BET = 500
-
     def betRequest(self, game_state):
         print (game_state)
         in_action=game_state['in_action']
         me=game_state['players'][in_action]
         current_buy_in=game_state['current_buy_in']
 
+        self.max_bet = 500
+
         cards = me['hole_cards']
         ccards = game_state['community_cards'] if 'community_cards' in game_state else 0
+
+        # Empty own Cards
         self.rank1 = None
         self.rank2 = None
         self.suit1 = None
         self.rank2 = None
+
+        # Define best hand
+        self.hand_flop = None
+        self.hand_turn = None
+        self.hand_river = None
+
+        # Definde Array for all Commuity Cards
+        self.cranks = []
+        self.csuits = []
+
+        # Set Ranks and Suits in Array
+        if len(ccards) == 0:
+            for ccard in ccards:
+                self.cranks.append(ccard['rank'])
+                self.csuits.append(ccard['suit'])
+
+        # Set own Cards to array
+        self.cranks.append(self.rank1)
+        self.cranks.append(self.rank2)
+        self.csuits.append(self.suit1)
+        self.csuits.append(self.suit2)
+
         if len(cards) == 2 and 'community_cards' in game_state:
 
+            # Play before Flop
             if len(ccards) == 0:
 
+                # Set Ammount for Raise & Call
                 self.minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
+                self.ammount_call = current_buy_in - me['bet']
 
+                # Set rank & suit for own hands
                 self.rank1 = cards[0]['rank']
                 self.rank2 = cards[1]['rank']
                 self.suit1 = cards[0]['suit']
                 self.suit2 = cards[1]['suit']
 
+                # Set calculated value for own cards
                 value = calc_cards.get_value(cards)
+
+                # Double Aces
                 if self.rank1 == self.rank2 and self.rank1 == 'A':
-                    #self.log_raise()
                     return  self.minimum_raise + 100
+
+                # Pair of Kings, Queens & Jacks
                 elif self.rank1 == self.rank2 and (self.rank1 == 'K' or self.rank1 == 'Q' or self.rank1 == 'J'):
-                    #self.log_raise()
                     return  self.minimum_raise + 75
+
+                # Play pair of low cards
                 elif self.rank1 == self.rank2:
-                    #self.log_raise()
                     if self.minimum_raise < 50:
                         return 50
+
+                # Play "pair" of suits
                 elif self.suit1 == self.suit2:
-                    #self.log_raise()
                     if self.minimum_raise < 20:
                         return 20
+
+                # Play low cards with one Ace
                 elif value < 8 and (self.rank1 != 'A' or self.rank2 != 'A'):
-                    #self.log_raise()
                     if self.minimum_raise < 10:
                         return 10
+
+
+                #elif current_buy_in < self.max_bet:
+                #    return self.ammount_call
+
+            # Play Flop
             elif  len(ccards) == 3:
-                # allways check
-                return 0
+                if self.rank1 != self.rank2:
+                    for x in [self.rank1, self.rank2]:
+                        if self.cranks.count(x) == 3:
+                            return 100
+                        if self.cranks.count(x) == 4:
+                            return 500
+                else:
+                    return 0
+
+            # Play Turn
             elif  len(ccards) == 4:
-                # allways check
-                return 0
+                if self.rank1 != self.rank2:
+                    for x in [self.rank1, self.rank2]:
+                        if self.cranks.count(x) == 3:
+                            return 100
+                        if self.cranks.count(x) == 4:
+                            return 500
+                else:
+                    return 0
+
+            # PLay River
             elif  len(ccards) == 5:
-                # allways check
-                return 0
+                if self.rank1 != self.rank2:
+                    for x in [self.rank1, self.rank2]:
+                        if self.cranks.count(x) == 3:
+                            return 100
+                        if self.cranks.count(x) == 4:
+                            return 500
+                else:
+                    return 0
 
+        # Always Check
         return 0
-        #amount = current_buy_in - me['bet']
-        #if amount > self.MAX_BET and self.rank1 != self.rank2:
-        #    print('CHECK only (HIGH bet)')
-        #    return 0 # fold
-
-        #if len(cards) > 1:
-        #    r = self.minimum_raise + random.random()*10 if random.random()>0.3 else 0
-        #    amount += int(r)
-        ##self.log_call(amount)
-        #return amount # always call
 
     def showdown(self, game_state):
         return True
-
-#    def log_check(self):
-#        if self.rank1:
-#            print('{} {}: CHECK only'.format(self.rank1, self.rank2))
-#        else:
-#            print('CHECK only')
-#
-#    def log_call(self, amount):
-#        if self.rank1:
-#            print('{} {}: CALL: {}'.format(self.rank1, self.rank2, amount))
-#        else:
-#            print('CALL: {}'.format(amount))
-#
-#    def log_raise(self, amount):
-#        if self.rank1:
-#            print('{} {}: BET: {}'.format(self.rank1, self.rank2, amount))
-#        else:
-#            print('BET: {}'.format(amount))
