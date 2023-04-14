7c12acddb86b5ed6f0d0b83d1c9af39e68906d00
FSchwehla <florian.schwehla@datev.de>
2023-04-14 11:48:36 +0200

    Wip

--- player.py
+++ player.py
@@ -13,53 +13,50 @@ class Player:
         current_buy_in=game_state['current_buy_in']
 
         cards = me['hole_cards']
-        ccards = game_state['community_cards']
+        ccards = game_state['community_cards'] if 'community_cards' in game_state else 0
         self.rank1 = None
         self.rank2 = None
         self.suit1 = None
         self.rank2 = None
-        if len(cards) == 2 and len(ccards) == 0:
+        if len(cards) == 2 and 'community_cards' in game_state:
 
-            self.minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
+            if len(ccards) == 0:
 
-            self.rank1 = cards[0]['rank']
-            self.rank2 = cards[1]['rank']
-            self.suit1 = cards[0]['suit']
-            self.suit2 = cards[1]['suit']
+                self.minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
 
-            value = calc_cards.get_value(cards)
-            if self.rank1 == self.rank2 and self.rank1 == 'A':
-                #self.log_raise()
-                return  self.minimum_raise + 100
-            elif self.rank1 == self.rank2 and (self.rank1 == 'K' or self.rank1 == 'Q' or self.rank1 == 'J'):
-                #self.log_raise()
-                return  self.minimum_raise + 75
-            elif self.rank1 == self.rank2:
-                #self.log_raise()
-                if self.minimum_raise < 50:
-                    return 50
-            elif self.suit1 == self.suit2:
-                #self.log_raise()
-                if self.minimum_raise < 20:
-                    return 20
-            elif value < 8 and (self.rank1 != 'A' or self.rank2 != 'A'):
-                #self.log_raise()
-                if self.minimum_raise < 10:
-                    return 10
+                self.rank1 = cards[0]['rank']
+                self.rank2 = cards[1]['rank']
+                self.suit1 = cards[0]['suit']
+                self.suit2 = cards[1]['suit']
 
-        elif len(cards) == 2 and len(ccards) == 3:
-            # allways check
-            return 0
-
-        elif len(cards) == 2 and len(ccards) == 4:
-            # allways check
-            return 0
-
-        elif len(cards) == 2 and len(ccards) == 5:
-            # allways check
-            return 0
-
-
+                value = calc_cards.get_value(cards)
+                if self.rank1 == self.rank2 and self.rank1 == 'A':
+                    #self.log_raise()
+                    return  self.minimum_raise + 100
+                elif self.rank1 == self.rank2 and (self.rank1 == 'K' or self.rank1 == 'Q' or self.rank1 == 'J'):
+                    #self.log_raise()
+                    return  self.minimum_raise + 75
+                elif self.rank1 == self.rank2:
+                    #self.log_raise()
+                    if self.minimum_raise < 50:
+                        return 50
+                elif self.suit1 == self.suit2:
+                    #self.log_raise()
+                    if self.minimum_raise < 20:
+                        return 20
+                elif value < 8 and (self.rank1 != 'A' or self.rank2 != 'A'):
+                    #self.log_raise()
+                    if self.minimum_raise < 10:
+                        return 10
+            elif  len(ccards) == 3:
+                # allways check
+                return 0
+            elif  len(ccards) == 4:
+                # allways check
+                return 0
+            elif  len(ccards) == 5:
+                # allways check
+                return 0
 
 
         #amount = current_buy_in - me['bet']
